4e272ffe7043ff7e3634c644005654b02a804b6b
henry <101145848+h-aig@users.noreply.github.com>
2026-06-24 20:14:07 +0100

    add deps

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿snAPI (pronounced 'snappy') is an API for social news platforms, allowing you to gather stories, comments, and more!
+
+Currently under development are implementations for both Hacker News and Lobste.rs.
---
+++ main.py
@@ -0,0 +1,21 @@
+﻿import requests
+from fastapi import FastAPI
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # customize this to only allow certain domains!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hn")
+def get_hn_data():
+    """Get a hacker news story.""" # this is a docstring
+    return  {requests.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').text}
+
+uvicorn.run(app)
